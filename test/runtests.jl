8698a39c5551743eb37c7dd401f0e9ea3069f2af
Uwe Fechner <fechner@aenarete.eu>
2022-09-04 11:36:56 +0200

    More tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,8 @@
 using Test, Compress
 
-cd("..")
+if basename(pwd()) == "test"
+    cd("..")
+end
 
 const filename="log_8700W_8ms.csv"
 outdir = mktempdir()
@@ -13,8 +15,20 @@ outdir = mktempdir()
 end
 
 @testset "compress:" begin
+    local sout, sorig
     input = joinpath(outdir, filename)
     output = joinpath(outdir, filename) * ".xz"
     bytes = compress(input, output)
     @test bytes == 3618752
+    open(output) do out
+        sout = read(out, String)
+    end
+    orig = joinpath("data", filename) * ".xz"
+    open(orig) do or
+        sorig = read(or, String)
+    end
+    # test that the file that was first decompressed and than compressed is identical with the original file
+    @test sout == sorig
 end
+
+println(outdir)
