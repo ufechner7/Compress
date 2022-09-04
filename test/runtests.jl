c8fd9fbcf07f41e788c0fe9e0e2fcde1fe89015b
Uwe Fechner <fechner@aenarete.eu>
2022-09-04 09:53:15 +0200

    Improve testing

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,8 +2,20 @@ using Test, Compress
 
 cd("..")
 
+# TODO: use mktempdir to create output folder
+
 const filename=joinpath("data", "log_8700W_8ms.csv")
 
-input = filename * ".xz"
-output = filename
-decompress(input, output)
+@testset "decompress:" begin
+    input = filename * ".xz"
+    output = filename
+    bytes = decompress(input, output)
+    @test bytes == 12758112
+end
+
+@testset "compress:" begin
+    input = filename
+    output = filename * ".xz"
+    bytes = compress(input, output)
+    @test bytes == 3618752
+end
